0a696cdec64b2b8a75a662c5d15c7cefe8d78d83
alxs1dd93 <alxs1dd93@gmail.com>
2024-03-28 21:03:33 +0700

    main add r and connect

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	_ "go-jwt/controllers"
+	"go-jwt/controllers"
 	"go-jwt/database"
-	_ "go-jwt/middlewares"
+	"go-jwt/middlewares"
 	"go-jwt/models"
 	"log"
 
-	_ "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+
 )
 
+// main is the entry point of the program.
+// It initializes the database, sets up the router and starts the server.
 func main() {
 	// Initialize the database
 	err := database.InitDatabase()
@@ -20,4 +23,38 @@ func main() {
 	// Automigrate the User model
 	// AutoMigrate() automatically migrates our schema, to keep our schema upto date.
 	database.GlobalDB.AutoMigrate(&models.User{})
+	// Set up the router
+	r := setupRouter()
+	// Start the server
+	r.Run(":8081")
+}
+
+// setupRouter sets up the router and adds the routes.
+func setupRouter() *gin.Engine {
+	// Create a new router
+	r := gin.Default()
+	// Add a welcome route
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Welcome To This Website")
+	})
+	// Create a new group for the API
+	api := r.Group("/api")
+	{
+		// Create a new group for the public routes
+		public := api.Group("/public")
+		{
+			// Add the login route
+			public.POST("/login", controllers.Login)
+			// Add the signup route
+			public.POST("/signup", controllers.Signup)
+		}
+		// Add the signup route
+		protected := api.Group("/protected").Use(middlewares.Authz())
+		{
+			// Add the profile route
+			protected.GET("/profile", controllers.Profile)
+		}
+	}
+	// Return the router
+	return r
 }
