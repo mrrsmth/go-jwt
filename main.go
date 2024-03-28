e157eb11dd4537adcb201539b277f7d7865df693
alxs1dd93 <alxs1dd93@gmail.com>
2024-03-28 20:22:50 +0700

    check db

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	_ "go-jwt/controllers"
+	"go-jwt/database"
+	_ "go-jwt/middlewares"
+	"go-jwt/models"
+	"log"
 
+	_ "github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Initialize the database
+	err := database.InitDatabase()
+	if err != nil {
+		// Log the error and exit
+		log.Fatalln("could not create database", err)
+	}
+	// Automigrate the User model
+	// AutoMigrate() automatically migrates our schema, to keep our schema upto date.
+	database.GlobalDB.AutoMigrate(&models.User{})
 }
